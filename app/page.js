cdc35c9d01dbc1cc6fe65779419c7343a7e59b7b
bilal-waleed <bilalwaleed246@gmail.com>
2025-07-31 01:51:29 +0500

    zcrum

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+    </div>
+  );
+}
