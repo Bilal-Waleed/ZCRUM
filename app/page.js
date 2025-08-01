95cb945c93509e65b0ddf3ec29849d4dc9e457eb
bilal-waleed <bilalwaleed246@gmail.com>
2025-08-02 03:46:49 +0500

    update code

--- app/page.js
+++ app/page.js
@@ -1,16 +1,7 @@
 export default function Home() {
-  let a; 
   return (
     <div>
       <h1>Home</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      />
     </div>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,40 @@
+import { SignedOut, SignedIn, SignInButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+import { PenBox } from 'lucide-react'
+import UserMenu from './user-menu'
+
+const Header = () => {
+  return (
+    <header className='container max-auto'>
+        <nav className='flex items-center justify-between py-6 px-4'>
+            <Link href="/">
+                <Image src="/logo2.png" width={200} height={56} alt="zcrum-logo" className='h-10 object-contain w-auto' />
+            </Link> 
+        <div className='flex items-center gap-4'>
+            <Link href= '/project/crate'>
+                <Button variant="destructive" className='flex items-center gap-2'>
+                    <PenBox size={20} />
+                    <span>Create project</span>
+                </Button>
+            </Link>
+
+            <SignedOut>
+                <SignInButton forceRedirectUrl='/onboarding'>
+                    <Button variant={"outline"}>Login</Button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserMenu/>
+            </SignedIn>
+        </div>
+        </nav>
+    </header>
+   
+
+  )
+}
+
+export default Header
