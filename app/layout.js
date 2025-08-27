28bb5665f4b12dfc6b6b9cbfd05523da7c87483c
bilal-waleed <bilalwaleed246@gmail.com>
2025-08-27 23:51:12 +0500

    code update

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { shadesOfPurple } from "@clerk/themes";
 import { Toaster } from "sonner";
-import { Header } from "@/components/Header";
+import { Header } from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
